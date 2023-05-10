e106bcd9ad28287970a047734cfc4c904f4fb517
Prabina Regmi <git@prabina.us>
2023-05-09 22:33:40 -0600

    comple html, css and js

---
+++ app.js
@@ -0,0 +1,69 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6560f222339898411c41b679d76189dd&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=6560f222339898411c41b679d76189dd&query"';
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+//   Get initial movies
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = " ";
+
+  movies.forEach((movie) => {
+    const { original_title, backdrop_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `<img src= "${
+      IMG_PATH + backdrop_path
+    }" alt="${original_title}" srcset="" />
+        <div class="movie-info">
+          <h3>${original_title}</h3>
+          <span class="${getClassbyVote(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>`;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassbyVote(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+// Search
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+    window.location.reload();
+
+    search.value = "";
+  } else {
+  }
+});
